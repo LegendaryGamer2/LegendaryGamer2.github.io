a565eb6e021de78f4395b354ad4afe1b1ae5a47e
LegendaryGamer2 <90922505+LegendaryGamer2@users.noreply.github.com>
2021-09-27 08:49:19 -0600

    fixed all issues

--- ball_tower_defense/sketch.js
+++ ball_tower_defense/sketch.js
@@ -56,15 +56,15 @@ function preload() {
 // enemy related behaviour
 let length = 0;
 class Ball {
-  constructor(x, y, r, h) {
+  constructor(x, y, r) {
     this.x = x;
     this.y = y;
     this.r = r;
-    this.h = health
+    this.h = health;
   }
   //   Creates the actual circle
   circleCreator() {
-    fill(255)
+    fill(255);
     circle(this.x, this.y, this.r);
   }
   // Make the ball move accross the track
@@ -112,7 +112,7 @@ class Ball {
         console.log("done");
         
         if (loop2 === 2000) {
-          lives -= 1
+          lives -= 1;
           loop1 = -1;
           changer = 0;
           for (let ball2 of balls) {
@@ -177,75 +177,75 @@ function draw() {
   background(bColor);
   // creates the game if any key is pressed
   if (starter === 0){
-  textSize(200);
-  text('Ball Tower Defense', windowWidth / 2 - 900, windowHeight / 2 - 200);
-  textSize(50);
-  text('Each tower costs 100 points and the balls health increases by 1 each time it dies', windowWidth / 2 - 900, windowHeight / 2 );
-  textSize(50);
-  text('To start the game press any key', windowWidth / 2 - 900, windowHeight / 2 + 100);
-  if (keyIsPressed === true){
-    keyIsPressed = false;
-    window.setInterval(start, 3000);
+    textSize(200);
+    text("Ball Tower Defense", windowWidth / 2 - 900, windowHeight / 2 - 200);
+    textSize(50);
+    text("Each tower costs 100 points and the balls health increases by 1 each time it dies", windowWidth / 2 - 900, windowHeight / 2 );
+    textSize(50);
+    text("To start the game press any key", windowWidth / 2 - 900, windowHeight / 2 + 100);
+    if (keyIsPressed === true){
+      keyIsPressed = false;
+      window.setInterval(start, 3000);
     
-  }
+    }
   }
   else if (starter === 1){
-  frameRate(60);
-  // Enemy related objects
-  pathway();
-  speedChanger();
-  createEnemy();
-  restart();
+    frameRate(60);
+    // Enemy related objects
+    pathway();
+    speedChanger();
+    createEnemy();
+    restart();
 
-  // Image setting up
-  imageMode(CENTER);
-  image(testimage, windowWidth / 1.3, windowHeight / 1.3);
-  if (state_placing === 1){
+    // Image setting up
+    imageMode(CENTER);
+    image(testimage, windowWidth / 1.3, windowHeight / 1.3);
+    if (state_placing === 1){
     
-    image(testimage, mouseX, mouseY);
-    fill(0, 255, 0, 100);
-    circle(mouseX, mouseY, 150);
+      image(testimage, mouseX, mouseY);
+      fill(0, 255, 0, 100);
+      circle(mouseX, mouseY, 150);
     }
 
 
-  for(let block of towers){
-    image(testimage, block.x, block.y);
-    fill(0, 255, 0, 100);
-    circle(block.x, block.y, 150);
+    for(let block of towers){
+      image(testimage, block.x, block.y);
+      fill(0, 255, 0, 100);
+      circle(block.x, block.y, 150);
 
-    for (let enemyH of balls){
-    hit = collidePointCircle(enemyH.x, enemyH.y, block.x, block.y, 150);
-    if (hit === true){
-      enemyH.h -= .05;
-    }
-      // ball killed 
-    if (enemyH.h <= 0){
-      enemyH.restart();
-      addHealth += 1;
-      money += 100;
-    }
+      for (let enemyH of balls){
+        hit = collidePointCircle(enemyH.x, enemyH.y, block.x, block.y, 150);
+        if (hit === true){
+          enemyH.h -= 0.05;
+        }
+        // ball killed 
+        if (enemyH.h <= 0){
+          enemyH.restart();
+          addHealth += 1;
+          money += 100;
+        }
 
-    }  
-  }
+      }  
+    }
 
   
-  textSize(100);
-  fill(0);
-  text(money, windowWidth/3, 100);
-  text(lives, windowWidth - 200, 100);
-  if (lives === 0){
-    starter = 2;
-  }
+    textSize(100);
+    fill(0);
+    text(money, windowWidth/3, 100);
+    text(lives, windowWidth - 200, 100);
+    if (lives === 0){
+      starter = 2;
+    }
   }
   else if (starter === 2){
     textSize(50);
-    text('You Lose, hit refresh to restart', windowWidth / 2 - 900, windowHeight / 2);
-    }
+    text("You Lose, hit refresh to restart", windowWidth / 2 - 900, windowHeight / 2);
+  }
   
 }
 
 function start (){
-  starter = 1
+  starter = 1;
 }
 
 
@@ -281,29 +281,29 @@ function restart(){
 function pathway() {
   if (starter === 1){
   //Setting up path of the enemies
-  x1 = windowWidth / 10;
-  y1 = windowHeight / 3;
-  x2 = windowWidth / 1.5;
-  y2 = windowHeight / 1.5;
-  //First Row/ Entrance
-  line(x1, 0, x1, y1);
-  line(x1 + 50, 0, x1 + 50, y1 - 50);
+    x1 = windowWidth / 10;
+    y1 = windowHeight / 3;
+    x2 = windowWidth / 1.5;
+    y2 = windowHeight / 1.5;
+    //First Row/ Entrance
+    line(x1, 0, x1, y1);
+    line(x1 + 50, 0, x1 + 50, y1 - 50);
 
-  //Second Row
-  line(x1, y1, x2, y1);
-  line(x1 + 50, y1 - 50, x2 + 50, y1 - 50);
+    //Second Row
+    line(x1, y1, x2, y1);
+    line(x1 + 50, y1 - 50, x2 + 50, y1 - 50);
 
-  //Third Row
-  line(x2, y1, x2, y2);
-  line(x2 + 50, y1 - 50, x2 + 50, y2 + 50);
+    //Third Row
+    line(x2, y1, x2, y2);
+    line(x2 + 50, y1 - 50, x2 + 50, y2 + 50);
 
-  //Fourth Row
-  line(x2, y2, x1, y2);
-  line(x2 + 50, y2 + 50, x1 + 50, y2 + 50);
+    //Fourth Row
+    line(x2, y2, x1, y2);
+    line(x2 + 50, y2 + 50, x1 + 50, y2 + 50);
 
-  //Last Row/ Exit
-  line(x1, y2, x1, height);
-  line(x1 + 50, y2 + 50, x1 + 50, height);
+    //Last Row/ Exit
+    line(x1, y2, x1, height);
+    line(x1 + 50, y2 + 50, x1 + 50, height);
   }
 }
 // All the tower code
@@ -313,7 +313,7 @@ function mouseClicked(){
 
   }
   // placement of the tower
-  else if (state_placing === 1 && money >= 100 && ((mouseX > x1 + 50 && ((mouseY < y1 - 50 || mouseY > y1) && (mouseY > y2 + 50 || mouseY < y2))) || (mouseX < x1 && mouseY > 0 && mouseY < height) || (mouseX < x1 + 50 && (mouseY > y1 && mouseY < y2) || mouseX > x1 && (mouseY > y1 && mouseY < y2) ))){
+  else if (state_placing === 1 && money >= 100 && (mouseX > x1 + 50 && ((mouseY < y1 - 50 || mouseY > y1) && (mouseY > y2 + 50 || mouseY < y2)) || mouseX < x1 && mouseY > 0 && mouseY < height || (mouseX < x1 + 50 && (mouseY > y1 && mouseY < y2) || mouseX > x1 && (mouseY > y1 && mouseY < y2) ))){
     towers.push(new tower(mouseX, mouseY));
     money -= 100;
     state_placing = 0;
