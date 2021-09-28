4669a14cce0e90f6b98edf735c6b773d8d9cc440
LegendaryGamer2 <90922505+LegendaryGamer2@users.noreply.github.com>
2021-09-28 09:32:41 -0600

    Update sketch.js

--- neighbours/sketch.js
+++ neighbours/sketch.js
@@ -1,15 +1,32 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Grid Neighbours
 
+let gridDimensions = 10;
+let grid;
+let cellsize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  grid = createRandomArray(gridDimensions);
 }
 
 function draw() {
   background(220);
+}
+
+function createRandomArray(howLarge) {
+  let newArray = [];
+  for (let y = 0; y < howLarge; y++){
+    newArray.push([]);
+    for (let x = 0; x < howLarge; x++){
+      if (random(0,100) > 50){
+        newArray[y].push(0);
+      }
+    
+      else {
+        newArray[y].push(1);
+      }
+    }
+  }
+  return newArray;
+
 }
