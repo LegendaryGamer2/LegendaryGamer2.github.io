add2d429b97e13672e5537b4d481409ce56d8c14
LegendaryGamer2 <90922505+LegendaryGamer2@users.noreply.github.com>
2021-10-04 09:38:35 -0600

    Update sketch.js

--- sudoku/sketch.js
+++ sudoku/sketch.js
@@ -12,6 +12,7 @@ let initialGrid = [
   [0, 0, 0, 0, 4, 9, 2, 1, 0],
 ];
 
+let grid;
 let gridDimensions = 9;
 let cellSize;
 
@@ -22,10 +23,41 @@ function setup() {
   else {
     createCanvas(windowWidth, windowWidth);
   }
-
+  grid = initialGrid;
   cellSize = width/gridDimensions;
 }
 
 function draw() {
   background(220);
+  displayGrid();
+}
+
+function displayGrid() {
+  for(let y = 0; y < gridDimensions; y ++){
+    for(let x = 0; x < gridDimensions; x++){
+      fill("white");
+      strokeWeight(1);
+      // noStroke();
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+
+      if (grid[y][x] !== 0){
+        fill("black");
+        textSize(cellSize * 0.75);
+        textAlign(CENTER, CENTER);
+        text(grid[y][x], x * cellSize + cellSize/2, y * cellSize + cellSize/2);
+      }
+    }
+  }
+  drawCageLines();
+}
+
+
+function drawCageLines() {
+  strokeWeight(2);
+  for(let location = 0; location <= 9; location += 3){
+    // horizontal lines
+    line(0, location * cellSize, width, location* cellSize);
+    // vertical lines
+    line(location * cellSize, 0, location * cellSize, height);
+  }
 }
