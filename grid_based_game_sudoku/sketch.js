f4c3875cd7edd586d70b364b6fb0c9341e61d195
LegendaryGamer2 <90922505+LegendaryGamer2@users.noreply.github.com>
2021-10-08 10:57:58 -0600

    changed what the project is to be

---
+++ grid_based_game_sudoku/sketch.js
@@ -0,0 +1,84 @@
+// Sudoku Text File Demo
+
+let initialGrid;
+
+let grid;
+let gridDimensions = 9;
+let cellSize;
+let gridInArray;
+
+function preload() {
+  initialGrid = loadStrings("assets/level2.txt");
+}
+
+function setup() {
+  if (windowWidth > windowHeight){
+    createCanvas(windowHeight * 0.8, windowHeight * 0.8);
+  }
+  else {
+    createCanvas(windowWidth * 0.8, windowWidth * 0.8);
+  }
+
+  initialGrid = convertedToIntGrid(initialGrid);
+
+  grid = initialGrid;
+  cellSize = width/gridDimensions;
+}
+
+function convertedToIntGrid(initialGrid){
+  // assume rectangular array
+  let rows = initialGrid.length;
+  let cols = initialGrid[0].length;
+
+  let newGrid = [];
+  for (let y = 0; y<rows; y++){
+    newGrid.push([]);
+    for (let x=0; x<cols; x++){
+      newGrid[y].push( int(initialGrid[y][x]));
+    }
+  }
+  return newGrid;
+}
+
+
+function draw() {
+  background(220);
+  displayGrid();
+}
+
+
+function windowResized() {
+  setup();
+}
+
+function displayGrid() {
+  for(let y = 0; y < gridDimensions; y ++){
+    for(let x = 0; x < gridDimensions; x++){
+      
+      fill("white");
+      strokeWeight(1);
+      // noStroke();
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+
+      if (grid[y][x] !== 0){
+        
+        fill("black");
+        textSize(cellSize * 0.75);
+        textAlign(CENTER, CENTER);
+        text(grid[y][x], x * cellSize + cellSize/2, y * cellSize + cellSize/2);
+      }
+    }
+  }
+  drawCageLines();
+}
+
+
+function drawCageLines() {
+  strokeWeight(2);
+  for(let location = 0; location <= 9; location += 3){
+    // horizontal lines
+    line(0, location * cellSize, width, location* cellSize);
+    // vertical lines
+    line(location * cellSize, 0, location * cellSize, height);
+  }
+}
