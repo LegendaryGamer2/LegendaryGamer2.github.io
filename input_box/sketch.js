90364e9a983b4a5b7559e0dbe6898bf32f234958
LegendaryGamer2 <90922505+LegendaryGamer2@users.noreply.github.com>
2021-10-12 10:15:15 -0600

    input box that deletes the text afterwards

---
+++ input_box/sketch.js
@@ -0,0 +1,33 @@
+let input, button, greeting;
+let inputedNum;
+
+function setup() {
+  // create canvas
+  createCanvas(400, 400);
+
+  input = createInput();
+  input.size(10,10);
+  input.position(width/4+20, height/4+10);
+  button = createButton("");
+  button.size(10,16);
+  button.position(width/4+5, height/4+10);
+  button.mousePressed(displayText);
+
+  greeting = createElement("h6","Insert Number");
+  greeting.position(width/4, height/4);
+
+  textAlign(CENTER);
+  textSize(10);
+}
+
+function displayText(){
+  let name = input.value();
+  input.value("");
+  name = int(name);
+  if (Number.isInteger(name) === true){
+    console.log(name);
+  }
+  else{
+    console.log("that is not a number");
+  }
+}
