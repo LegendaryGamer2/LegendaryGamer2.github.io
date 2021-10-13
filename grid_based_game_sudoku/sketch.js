6fa943835b3099b583bd7400be16a4c5db3177df
LegendaryGamer2 <90922505+LegendaryGamer2@users.noreply.github.com>
2021-10-13 10:22:36 -0600

    Update sketch.js

--- grid_based_game_sudoku/sketch.js
+++ grid_based_game_sudoku/sketch.js
@@ -4,16 +4,29 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let initialGrid;
+
 let input, button, greeting;
 let inputedNum;
 let grid;
 let gridDimensions = 9;
 let cellSize;
 let gridInArray;
-function preload() {
-  initialGrid = loadStrings("assets/level2.txt");
-}
+
+let initialGrid = [
+  [0, 2, 4, 3, 8, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 6, 0, 0, 7],
+  [0, 5, 8, 0, 0, 0, 4, 0, 0],
+  [4, 0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 7, 0, 5, 0, 0, 0],
+  [0, 0, 0, 0, 2, 0, 0, 0, 8],
+  [0, 0, 1, 0, 0, 0, 6, 7, 0],
+  [3, 0, 0, 5, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 4, 9, 2, 1, 0],
+];
+
+// function preload() {
+//   initialGrid = loadStrings("assets/level2.txt");
+// }
 
 function setup() {
   if (windowWidth > windowHeight){
@@ -87,11 +100,10 @@ function displayText(){
 
 function mousePressed(){
   if (mouseX <= width && mouseY <= height){
-    let cellx = Math.floor(mouseX/gridDimensions);
-    let celly = Math.floor(mouseY/gridDimensions);
-
-    let h = grid[celly][cellx].pop();
-    
+    let cellx = Math.floor(mouseX/gridDimensions) - 1;
+    let celly = Math.floor(mouseY/gridDimensions) - 1;
+    console.log(cellx, celly);
+    console.log(initialGrid[celly][cellx]);
   }
 }
 
