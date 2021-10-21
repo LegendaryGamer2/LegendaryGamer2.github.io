45e0454db26aee088c5f5a00532107d844e0f2b3
LegendaryGamer2 <90922505+LegendaryGamer2@users.noreply.github.com>
2021-10-21 09:49:48 -0600

    pushing boxes

--- matter_testing/sketch.js
+++ matter_testing/sketch.js
@@ -7,29 +7,30 @@ let Engine = Matter.Engine,
   
 let engine;
 let world;
-let boxA;
-let boxB;
+let boxes = [];
 let ground;
 
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(400, 400);
   engine = Engine.create();
   world = engine.world;
-  boxA = Bodies.rectangle(400, 200, 80, 80);
-  boxB = Bodies.rectangle(450, 50, 80, 80);
-  ground = Bodies.rectangle(0, 610, width, 80, {
+  Engine.run(engine);
+  let options = {
     isStatic: true
-  });
-  Matter.Runner.run(engine);
-  World.add(engine.world, [boxA, boxB, ground]);
+  };
+  ground = Bodies.rectangle(200, height-20, width, 10, options);
+  World.add(world, ground);
 }
 
+function mousePressed(){
+  boxes.push(new Box(mouseX, mouseY, 50, 50));
+}
 
 function draw() {
   background(0);
-
-  fill(255);
-  rect(boxA.position.x, boxA.position.y, 80, 80);
-  rect(boxB.position.x, boxB.position.y, 80, 80);
-  rect(ground.position.x, ground.position.y, width, 10);
+  for (let i = 0; i < boxes.length; i++){
+    boxes[i].show();
+  }
+  
 }
